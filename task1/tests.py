f568dc828c6c25b4e7223b3e752d52b064111a43
User <ntumasevic@gmail.com>
2024-05-08 10:42:32 +0300

    git initial

--- task1/tests.py
+++ task1/tests.py
@@ -59,20 +59,6 @@ class Task2Test(unittest.TestCase):
 
 
 class Task3Test(unittest.TestCase):
-    def setUp(self):
-        self.documents = [
-            {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-            {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-            {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
-            {"type": "driver license", "number": "5455 028765", "name": "Василий Иванов"},
-        ]
-
-        self.directories = {
-            '1': ['2207 876234', '11-2', '5455 028765'],
-            '2': ['10006'],
-            '3': []
-        }
-
     def test_get_name_existing_document(self):
         self.assertEqual(get_name("10006"), "Аристарх Павлов")
 
